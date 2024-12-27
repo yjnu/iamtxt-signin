922e331ec1e538d7270f92350b3880b07c48933c
leone <709890764@qq.com>
2024-12-27 11:25:47 +0800

    update headers

--- hifini_sign.py
+++ hifini_sign.py
@@ -9,23 +9,49 @@ wechatSendkey = os.environ.get('SENDKEY')
 signUrl = "https://www.hifini.com/sg_sign.htm"
 
 signHeaders = {
-    "accept"          : "text/plain, */*; q=0.01",
-    "accept-encoding" : "gzip, deflate, br, zstd",
-    "accept-language" : "zh-CN,zh;q=0.9,en;q=0.8",
-    "content-length"  : "69",
-    "content-type"    : "application/x-www-form-urlencoded; charset=UTF-8",
-    "cookie"          : hifiniCookie,
-    "origin"          : "https://www.hifini.com",
-    "referer"         : "https://www.hifini.com/",
-    "x-requested-with": "XMLHttpRequest",
-    "user-agent"      : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    "authority"         : "www.hifini.com",
+    "accept"            : "text/plain, */*; q=0.01",
+    "accept-encoding"   : "gzip, deflate, br, zstd",
+    "accept-language"   : "zh-CN,zh;q=0.9,en;q=0.8",
+    "content-length"    : "69",
+    "content-type"      : "application/x-www-form-urlencoded; charset=UTF-8",
+    "cookie"            : hifiniCookie,
+    "origin"            : "https://www.hifini.com",
+    "priority"          : "u=1,i",
+    "referer"           : "https://www.hifini.com/sg_sign.htm",
+    'sec-ch-ua'         : '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    'sec-ch-ua-mobile'  : '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest'    : 'empty',
+    'sec-fetch-mode'    : 'cors',
+    'sec-fetch-site'    : 'same-origin',
+    "x-requested-with"  : "XMLHttpRequest",
+    "user-agent"        : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+}
+
+getHeaders = {
+    "authority"         : "www.hifini.com",
+    'accept'            : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language'   : 'zh-CN,zh;q=0.9,en;q=0.8',
+    'cookie'            : hifiniCookie,
+    'priority'          : 'u=0,i',
+    'referer'           : signUrl,
+    'sec-ch-ua'         : '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    'sec-ch-ua-mobile'  : '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest'    : 'document',
+    'sec-fetch-mode'    : 'navigate',
+    'sec-fetch-site'    : 'same-origin',
+    'sec-fetch-user'    : '?1',
+    "user-agent"        : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
 }
 
 def sign_post():
-    response = requests.get(signUrl, headers=signHeaders)
+    response = requests.get(signUrl, headers=getHeaders)
     # print(response.text)
     pattern = r'var sign = "(.+)"'
     matches = re.findall(pattern, response.text)
+    # print(matches)
     if matches:
         sign_code = matches[0]
     else:
@@ -75,6 +101,7 @@ def judge_sign(ret_dict):
 
 if __name__ == '__main__':
     signRet = sign_post()
+    # print(signRet)
     signRet = json.loads(signRet)
     sctitle, scmessage = judge_sign(signRet)
     ret = sc_send(wechatSendkey, sctitle, scmessage)
